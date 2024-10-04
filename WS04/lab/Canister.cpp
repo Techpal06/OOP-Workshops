b1f1e3c2e4e167e8e9b63fb6edd6cf7c7c6c88d7
Gopal Sapkota <gsapkota@myseneca.ca>
2024-10-04 09:16:00 -0400

    workshop4 completed

--- WS04/lab/Canister.cpp
+++ WS04/lab/Canister.cpp
@@ -4,52 +4,136 @@
 using namespace std;
 namespace seneca {
 
-   const double PI = 3.14159265;
-   
+    const double PI = 3.14159265;
 
-   bool Canister::isEmpty() const {
-      return m_contentVolume < 0.00001;
-   }
 
-   bool Canister::usable() const
-   {
-      return m_height >= 10 && m_height <= 40 && m_diameter >= 10 && m_diameter <= 30;
-   }
+    bool Canister::isEmpty() const {
+        return m_contentVolume < 0.00001;
+    }
 
-   void Canister::setToUnusable(){
-      freeMem(m_contentName);
-      m_diameter = m_height = -1.0;
-   }
+    bool Canister::usable() const
+    {
+        return m_height >= 10 && m_height <= 40 && m_diameter >= 10 && m_diameter <= 30;
+    }
 
-   double Canister::capacity() const {
-      return PI * (m_height-0.267) * (m_diameter/2) * (m_diameter / 2);
-   }
+    void Canister::setToUnusable() {
+        freeMem(m_contentName);
+        m_diameter = m_height = -1.0;
+    }
 
-      bool Canister::hasSameContent(const Canister& C)const {
-      return m_contentName && C.m_contentName && strcmp(m_contentName, C.m_contentName) == 0;
-   }
+    double Canister::capacity() const {
+        return PI * (m_height - 0.267) * (m_diameter / 2) * (m_diameter / 2);
+    }
 
-   double Canister::volume() const {
-      return m_contentVolume;
-   }
+    bool Canister::hasSameContent(const Canister& C)const {
+        return m_contentName && C.m_contentName && strcmp(m_contentName, C.m_contentName) == 0;
+    }
 
-   std::ostream& Canister::display() const {
-      cout.setf(ios::fixed);
-      cout.precision(1);
-      cout.width(7);
-      cout << capacity() << "cc (";
-      cout.width(4);
-      cout << m_height << "x";
-      cout.width(4);
-      cout << m_diameter << ") Canister";
-      if (!usable()) {
-         cout << " is  Unusable   recycle!";
-      }
-      else {
-         cout << " of ";
-         cout.width(7);
-         cout << volume() << "cc   " << (m_contentName?m_contentName:"Sanetized and Empty");
-      }
-      return cout;
-   }
+    double Canister::volume() const {
+        return m_contentVolume;
+    }
+
+    Canister::Canister()
+    {
+        m_contentName = nullptr;
+        m_diameter = 10.0;
+        m_height = 13.0;
+        m_contentVolume = 0.0;
+    }
+
+    Canister::Canister(const char* contentName)
+    {
+        m_contentName = nullptr;
+        m_diameter = 10.0;
+        m_height = 13.0;
+        m_contentVolume = 0.0;
+        alocpy(m_contentName, contentName);
+    }
+
+    Canister::Canister(double height, double diameter, const char* contentName)
+    {
+        m_contentName = nullptr;
+        m_contentVolume = 0.0;
+        m_height = height;
+        m_diameter = diameter;
+        if (usable()) {
+            alocpy(m_contentName, contentName);
+        }
+    }
+
+    Canister& Canister::clear()
+    {
+        freeMem(m_contentName);
+        m_contentVolume = 0.0;
+        return *this;
+    }
+
+    Canister& Canister::setContent(const char* contentName)
+    {
+        if (contentName && contentName[0] && usable()) {
+            if (!(m_contentName && m_contentName[0]) || isEmpty()) {
+                alocpy(m_contentName, contentName);
+            }
+            else if (strcmp(m_contentName, contentName) != 0) {
+                setToUnusable();
+            }
+        }
+        return *this;
+    }
+
+    Canister& Canister::pour(double quantity)
+    {
+        if (usable()) {
+            if (quantity > 0 && (quantity + m_contentVolume) < capacity()) {
+                m_contentVolume += quantity;
+            }
+            else {
+                setToUnusable();
+            }
+        }
+        return *this;
+    }
+
+    Canister& Canister::pour(Canister& can)
+    {
+
+        if (usable()) {
+            if (can.volume() > (capacity() - volume())) {
+                can.m_contentVolume -= (capacity() - volume());
+                m_contentVolume = capacity();
+            }
+            else {
+                m_contentVolume += can.volume();
+                can.m_contentVolume = 0.0;
+            }
+
+            setContent(can.m_contentName);
+        }
+        return *this;
+    }
+
+    Canister::~Canister()
+    {
+        freeMem(m_contentName);
+    }
+
+    std::ostream& Canister::display() const {
+        cout.setf(ios::fixed);
+        cout.precision(1);
+        cout.width(7);
+        cout << capacity() << "cc (";
+        cout.width(4);
+        cout << m_height << "x";
+        cout.width(4);
+        cout << m_diameter << ") Canister";
+        if (!usable()) {
+            cout << " is  Unusable   recycle!";
+        }
+        else {
+            cout << " of ";
+            cout.width(7);
+            cout << volume() << "cc   " << (m_contentName ? m_contentName : "Sanetized and Empty");
+        }
+        return cout;
+    }
 }
