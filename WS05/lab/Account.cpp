fa9fdecf0ecf1fdded6465a24160903e418f9807
Gopal Sapkota <gsapkota@myseneca.ca>
2024-10-09 18:29:27 -0400

    some implementation complete

--- WS05/lab/Account.cpp
+++ WS05/lab/Account.cpp
@@ -42,5 +42,79 @@ namespace seneca {
       return cout;
    }
 
+   Account::operator bool() const
+   {
+       return m_number >= 1000 && m_number <= 9999 && m_balance > 0;
+   }
+
+
+   Account::operator double() const
+   {
+       return m_balance;
+   }
+   bool Account::operator~() const {
+       return m_number == 0;
+    }
  
+   // Assign a new account number
+   Account& Account::operator=(int number) {
+       if (number >= 10000 && number <= 99999) {
+           m_number = number;
+           m_balance = 0.0;
+       }
+       return *this;
+   }
+
+   // Assign an account to another account
+   Account& Account::operator=(Account& other) {
+       if (this != &other && other) {  // Ensure not self-assignment and valid account
+           m_number = other.m_number;
+           m_balance = other.m_balance;
+       }
+       return *this;
+   }
+
+   // Move funds from one account to another (>>) 
+   Account& Account::operator>>(Account& other) {
+       if (this != &other && *this && other) {  // Valid accounts and not the same
+           other.m_balance += m_balance;
+           m_balance = 0.0;
+       }
+       return *this;
+   }
+
+   // Move funds from another account (<<) 
+   Account& Account::operator<<(Account& other) {
+       if (this != &other && *this && other) {  // Valid accounts and not the same
+           m_balance += other.m_balance;
+           other.m_balance = 0.0;
+       }
+       return *this;
+   }
+   Account& Account::operator+=(double amount)
+   {
+           if ( *this) {  // Ensure the amount is positive and the account is valid
+               m_balance += amount;
+           }
+           return *this;
+    
+
+   }
+   Account& Account::operator-=(double amount)
+   {
+       if (amount > 0 && m_balance >= amount && *this) {  // Ensure sufficient balance and valid account
+           m_balance -= amount;
+       }
+       return *this;
+   }
+   Account Account::operator+(const Account& other) const
+   {
+     
+           Account temp = *this;
+           if (temp && other) {
+               temp.m_balance += other.m_balance;
+           }
+           return temp;
+   
+   }
 }
--- WS05/lab/Account.h
+++ WS05/lab/Account.h
@@ -1,19 +1,33 @@
-#ifndef SENECA_ACCOUNT_H_
-#define SENECA_ACCOUNT_H_
-#include <iostream>
+    #ifndef SENECA_ACCOUNT_H_
+    #define SENECA_ACCOUNT_H_
+    #include <iostream>
 
-namespace seneca {
-   class Account {
-      int m_number;
-      double m_balance; 
-      void setEmpty();
-   public:
-      Account();
-      Account(int number, double balance);
-      std::ostream& display()const;
-  
-   };
+    namespace seneca {
+       class Account {
+          int m_number;
+          double m_balance; 
+          void setEmpty();
+       public:
+          Account();
+          Account(int number, double balance);
+          std::ostream& display()const;
 
 
-}
-#endif // SENECA_ACCOUNT_H_
+
+          explicit operator bool() const; 
+          explicit operator double() const; 
+          bool operator~() const;
+
+          Account& operator =(int number);
+          Account& operator = (Account& other);
+          Account& operator >> (Account& other);
+          Account& operator << (Account& other);
+          Account& operator += (double amount);
+          Account& operator -= (double amount);
+          Account operator+(const Account& other) const;
+
+       };
+
+
+    }
+    #endif // SENECA_ACCOUNT_H_
